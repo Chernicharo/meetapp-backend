c43822e717187a236b1f94e95fa86d552cf3341d
Roberto Chernicharo <foxtamega@gmail.com>
2019-07-03 23:45:31 -0300

    added users routes, models and migrations

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req,res) =>{
-  return res.json({text: "hi"});
-})
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'test1',
+    email: 'test1c@test',
+    password_hash: 'testhash',
+  });
+  return res.json(user);
+});
 
 export default routes;
